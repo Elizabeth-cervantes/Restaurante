c5cc5f6f23fcc6b1fc647242033e3f57e9a54afa
Elizabeth-cervantes <156109742+Elizabeth-cervantes@users.noreply.github.com>
2024-01-23 16:17:32 -0600

    se hacen cambios en el index

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+const comidas = [arroz,moleverde, molerojo,chilaquiles,carne-asada ];
+
+const bebidas = [cocacola, agua-de-frutas];
+
+let opciones = "Por favor elige una opcion de la lista de comidas:\n;"
+
